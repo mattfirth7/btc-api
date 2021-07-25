012754bec1daf2da0b962573d9aa4635de9e951e
mattfirth7 <mmf001x@yahoo.com>
2021-07-25 14:46:38 -0400

    Added getinfo method for testing on AWS

--- services/wallet/routes.js
+++ services/wallet/routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 var request = require('request');
 const dotenv = require('dotenv');
+const Client = require('bitcoin-core')
 dotenv.config();
 
 
 // Pulls Bitcoin username and password from .env files
 const USER = process.env.RPC_USER;
 const PASS = process.env.RPC_PASSWORD;
+
+const client = new Client({host: "18.234.174.222", username: USER, password: PASS, port: 8333});
 
 const headers = {
 	"content-type": "text/plain;"
@@ -16,21 +19,45 @@ const headers = {
 // Creates new set of routes for the wallet endpoints
 const walletRouter = express.Router();
 
+walletRouter.get('/getinfo', (req, res) => {
+	const dataString = `{"jsonrpc:"1.0", "id":"curltest", "method":"getinfo", "params":[] }`;
+	const options = {
+		url: `http://${USER}:${PASS}@127.0.0.1:8333`,
+		method: 'POST',
+		headers: headers,
+		body: dataString
+	};
+
+	callback = (error, response, body) => {
+		if (!error && response.statusCode == 200) {
+			const data = JSON.parse(body);
+			res.send(data);
+		}
+	};
+
+	request(options, callback);
+});
+
 
 // New route /create with a JSON body to create a new wallet with a given name
 walletRouter.post("/create", (req, res) => {
+	console.log('received');
 
 	// Takes wallet name from request JSON body
 	const walletName = req.body.walletName;
 
 	// Adds the wallet name to JSON body format requried by Bitcoin Core
-	const dataString = `{"jsonrpc": "1.0","id": "curltext", "method": "createwallet", "params": ["${walletName}"]}`;
+	const dataString = `{"jsonrpc": "1.0","id": "curltest", "method": "createwallet", "params": ["${walletName}"]}`;
 
 	// Neatly organizes all request arguments
 	const options = {
-		url: `http://${USER}:${PASS}@54.167.84.55:8332/`,
+		url: `http://127.0.0.1:8333/`,
 		method: 'POST',
 		headers: headers,
+		auth: {
+			user: USER,
+			pass: PASS
+		},
 		body: dataString
 	};
 
@@ -55,7 +82,7 @@ walletRouter.get('/get', (req, res) => {
 
 	const privKeyDataString = `{"jsonrpc": "1.0", "id": "curltest", "method": "dumpprivkey", "params": ["${walletAddress}"] }`;
 	const privKeyOptions = {
-		url: `http://${USER}:${PASS}@54.167.84.55:8332/`,
+		url: `http://${USER}:${PASS}@18.234.174.222 :8332/`,
 		method: 'POST',
 		headers: headers,
 		body: privKeyDataString
