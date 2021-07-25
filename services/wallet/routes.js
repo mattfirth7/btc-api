58bd6fcd2f02b6845e19b22530a69ec9933941c9
mattfirth7 <mmf001x@yahoo.com>
2021-07-25 16:02:18 -0400

    Tweaks

--- services/wallet/routes.js
+++ services/wallet/routes.js
@@ -19,16 +19,16 @@ const walletRouter = express.Router();
 
 walletRouter.get('/getinfo', (req, res) => {
 	console.log('received');
-	const dataString = `{"jsonrpc:"1.0", "id":"curltest", "method":"getblockchaininfo", "params":[] }`;
 	const options = {
-		url: `http://127.0.0.1:8332`,
+		url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
 		method: 'POST',
 		headers: headers,
-		auth: {
-			user: USER,
-			password: PASS
-		},
-		body: dataString
+		body: JSON.stringify({
+			jsonrpc: "1.0",
+			id: "curltest",
+			method: "getblockchaininfo",
+			params: []
+		})
 	};
 
 	callback = (error, response, body) => {
